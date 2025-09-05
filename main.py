2e7ab9fdaf1c7c71c9cd51e5414c80c388970295
lizard <code.yusif.v@gmail.com>
2025-09-05 17:42:18 +0400

    env_check function added

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ import os
 
 def env_check():
     env = ".env"
-    if not os.path.exists(env):
-        print(".env file not found.")
-        return
-    else:
-        print(".env is exist")
+    keys = ["OTX_API_KEY", "ABUSE_API_KEY", "ACH_API_KEY", "VT_API_KEY"]
 
-env_check()
+    if not os.path.exists(env):
+        print(".env file not found. You need to create an .env file.")
+        return False
+
+    with open(env, "r") as f:
+        content = f.read()
+
+    envstatus = True
+
+    for key in keys:
+        if f"{key}=" in content:
+            print(f"✅ {key} is ready")
+        else:
+            print(f"⚠️ {key} is missing")
+            envstatus = False
+
+    return envstatus
+
+if __name__ == "__main__":
+    print(env_check())
