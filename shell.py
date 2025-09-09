8b705f2a7357af7340a7e0a34e9fef972a2dd901
lizard <code.yusif.v@gmail.com>
2025-09-09 17:01:58 +0400

    minor additions

--- shell.py
+++ shell.py
@@ -1,21 +1,49 @@
 import subprocess
+import shlex
+from integrations import malwareBazaar
 
+def gethash(args):
+    result = subprocess.run(
+        ["sha256sum", *args],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    print(result.stdout.strip().split()[0])
+    malwareBazaar.mb_hash(result.stdout.strip().split()[0])
 
 def mimir():
     print("Welcome to Mimir. Type 'help' for commands, 'exit' to quit.")
     while True:
-        command = input("|> ").strip().lower()
+        raw = input("|> ").strip().lower()
+        if not raw:
+            continue
+
+        parts = shlex.split(raw)
+        command, *args = parts
+
         if command == "exit":
             print("Exiting Mimir...")
             break
         elif command == "help":
             print("Available commands: help, exit")
-        elif command == "":
-            continue
+        elif command == "hash":
+            if not args:
+                print("Usage: hash <filename>")
+                continue
+            elif "-h" in args:
+                hashindex = args.index("-h")
+                hashstring = args[hashindex + 1]
+                print(malwareBazaar.mb_hash(hashstring))
+                continue
+            try:
+                gethash(args)
+            except subprocess.CalledProcessError as e:
+                print(f"Error: {e.stderr.strip()}")
         else:
             try:
                 result = subprocess.run(
-                    command, capture_output=True, text=True, shell=True, check=True
+                    raw, capture_output=True, text=True, shell=True, check=True
                 )
                 print(result.stdout)
             except subprocess.CalledProcessError as e:
